d700e13eb2d0f4ad6596941024aedd101b7818c8
intelguasoft <hnrdiaz@gmail.com>
2021-05-26 20:25:13 -0600

    Se creo aplicación para ngx-image-cropper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ng-demo-app';
+
+    imgChangeEvt: any = '';
+    cropImgPreview: any = '';
+
+    onFileChange(event: any): void {
+        this.imgChangeEvt = event;
+    }
+    cropImg(e: ImageCroppedEvent) {
+        this.cropImgPreview = e.base64;
+    }
+
+    imgLoad() {
+        // display cropper tool
+    }
+
+    initCropper() {
+        // init cropper
+    }
+
+    imgFailed() {
+        // error msg
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ImageCropperModule } from 'ngx-image-cropper';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ImageCropperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
